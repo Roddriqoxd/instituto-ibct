43f3706b10da0ab0a392d04d453ac125f026def4
roddriqoxd@gmail.com <rodrigo>
2025-06-16 22:38:21 -0400

    carreras

---
+++ src/app/home-page/components/bartender/bartender.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-bartender',
+  imports: [
+    NgIf
+  ],
+  templateUrl: './bartender.component.html',
+  styleUrl: './bartender.component.css'
+})
+export class BartenderComponent {
+
+}
