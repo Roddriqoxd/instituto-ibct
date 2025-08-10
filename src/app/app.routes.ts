93fe4389c9fe8e2fb806647234957daa2b3f5d7f
roddriqoxd@gmail.com <rodrigo>
2025-08-10 08:52:17 -0400

    refactor capetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,68 +1,22 @@
 import {Routes} from '@angular/router';
-import {HomePageComponent} from './home-page/components/home-page/home-page.component';
-import {InicioPageComponent} from './home-page/components/inicio-page/inicio-page.component';
-import {DashboardComponent} from './dashboard/dashboard/components/dashboard/dashboard.component';
-import {StudentFormComponent} from './dashboard/dashboard/components/student-form/student-form.component';
-import {CoursesListComponent} from './dashboard/dashboard/components/courses-list/courses-list.component';
-import {StudentListComponent} from './dashboard/dashboard/components/student-list/student-list.component';
-import {CreateCourseComponent} from './dashboard/dashboard/components/create-course/create-course.component';
-import {PricesComponent} from './dashboard/dashboard/components/prices/prices.component';
-import {PaymentFormComponent} from './dashboard/dashboard/components/payment-form/payment-form.component';
-import {BartenderComponent} from './home-page/components/bartender/bartender.component';
-import {ComputacionComponent} from './home-page/components/computacion/computacion.component';
-import {EnsambladoComponent} from './home-page/components/ensamblado/ensamblado.component';
-import {DGraficoComponent} from './home-page/components/d-grafico/d-grafico.component';
-import {CelularesComponent} from './home-page/components/celulares/celulares.component';
-import {PagosListComponent} from './dashboard/dashboard/components/pagos-list/pagos-list.component';
-import {ProfesoresComponent} from './home-page/components/profesores/profesores.component';
+import {DashboardComponent} from './dashboard/components/dashboard/dashboard.component';
+import {StudentFormComponent} from './dashboard/components/student-form/student-form.component';
+import {CoursesListComponent} from './dashboard/components/courses-list/courses-list.component';
+import {StudentListComponent} from './dashboard/components/student-list/student-list.component';
+import {CreateCourseComponent} from './dashboard/components/create-course/create-course.component';
+import {PricesComponent} from './dashboard/components/prices/prices.component';
+import {PaymentFormComponent} from './dashboard/components/payment-form/payment-form.component';
+import {PagosListComponent} from './dashboard/components/pagos-list/pagos-list.component';
 import {
   IncomeExpensesListComponent
-} from './dashboard/dashboard/components/income-expenses-list/income-expenses-list.component';
+} from './dashboard/components/income-expenses-list/income-expenses-list.component';
 import {
   ExpensesIncomeFormComponent
-} from './dashboard/dashboard/components/expenses-income-form/expenses-income-form.component';
-import {PaymentListComponent} from './dashboard/dashboard/components/payment-list/payment-list.component';
-import {ReportsListComponent} from './dashboard/dashboard/components/reports-list/reports-list.component';
+} from './dashboard/components/expenses-income-form/expenses-income-form.component';
+import {PaymentListComponent} from './dashboard/components/payment-list/payment-list.component';
+import {ReportsListComponent} from './dashboard/components/reports-list/reports-list.component';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    component: HomePageComponent,
-    children: [
-      {
-        path: 'inicio',
-        component: InicioPageComponent,
-      },
-      {
-        path: 'bartender',
-        component: BartenderComponent,
-      },
-      {
-        path: 'computacion',
-        component: ComputacionComponent,
-      },
-      {
-        path: 'ensamblado-computadoras',
-        component: EnsambladoComponent,
-      },
-      {
-        path: 'diseño-grafico',
-        component: DGraficoComponent,
-      },
-      {
-        path: 'reparacion-celulares',
-        component: CelularesComponent,
-      },
-      {
-        path: 'docentes',
-        component: ProfesoresComponent,
-      },
-      {
-        path: '**',
-        redirectTo: 'inicio',
-      },
-    ]
-  },
   {
     path: 'dashboard',
     component: DashboardComponent,
@@ -119,6 +73,6 @@ export const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: 'home',
+    redirectTo: 'dashboard',
   }
 ];
